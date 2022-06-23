f3a3900d89aa52081fabd1669c66ce36e996401f
Calvin Ho <calvin.ch.ho@gmail.com>
2022-06-23 00:41:51 -0400

    Add AJAX GET to fetch tweets and add form validation to prevent tweets that are empty or over 140 characters

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,81 +1,27 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Mary",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@MaryOliver" },
-    "content": {
-      "text": "Tell me, what is it you plan to do with your one wild and precious life?"
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "James",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@JamesBaldwin" },
-    "content": {
-      "text": "To act is to be committed, and to be committed is to be in danger."
-    },
-    "created_at": 1461113959088
-  },
-  {
-    "user": {
-      "name": "Jane",
-      "avatars": "https://i.imgur.com/lRUnDgU.png",
-      "handle": "@JaneAusten" },
-    "content": {
-      "text": "Friendship is certainly the finest balm for the pangs of disappointed love."
-    },
-    "created_at": 1461113959088
-  }
-]
-
 $(document).ready(function() {
 
   const createTweetElement = function(tweetData) {
+    
+    tweetData.created_at = timeago.format(tweetData.created_at);
+
     return(
       `<article class="tweet">
-      <header>  
-      <div class="avatar-user">
-      <span><img src=${tweetData.user.avatars}></span>
-      <p class="username">${tweetData.user.name}</p>
-      </div>
-      <span class="handle">${tweetData.user.handle}</span>
-      </header>
-      <p class="tweet-content">${tweetData.content.text}</p>
-      <footer>
-      <span class="tweet-date">${tweetData.created_at}</span>
-      <div class="tweet-reactions">
-      <span class="flag"><i class="fa-solid fa-flag"></i></span>
-      <span class="retweet"><i class="fa-solid fa-retweet"></i></span>
-      <span class="heart"><i class="fa-solid fa-heart"></i></span>
-      </div>
-      </footer>
+        <header>  
+          <div class="avatar-user">
+            <span><img src=${tweetData.user.avatars}></span>
+            <p class="username">${tweetData.user.name}</p>
+          </div>
+          <span class="handle">${tweetData.user.handle}</span>
+        </header>
+        <p class="tweet-content">${tweetData.content.text}</p>
+        <footer>
+          <span class="tweet-date">${tweetData.created_at}</span>
+            <div class="tweet-reactions">
+              <span class="flag"><i class="fa-solid fa-flag"></i></span>
+              <span class="retweet"><i class="fa-solid fa-retweet"></i></span>
+              <span class="heart"><i class="fa-solid fa-heart"></i></span>
+            </div>
+        </footer>
       </article>`
     );
   }
@@ -86,28 +32,59 @@ $(document).ready(function() {
     // takes return value and appends it to the tweets container
     for (const tweet of tweets) {
       const $tweet = createTweetElement(tweet);
-      $('#tweet-container').append($tweet);
+      $('#tweet-container').prepend($tweet);
     }
   }
-  renderTweets(data);  
 
   //!!!what is "event"?
   $('#tweet-submit').submit(function (event) { 
-    const tweetEntered = $(this);
-    
-    //serialize tweet text for backend consumption
-    tweetQueryString = tweetEntered.serialize();
-
-    //send AJAX POST request
-    $post("/tweets/", tweetQueryString);
-    
-    // $.ajax({
-    //   type: "POST",
-    //   url: "/tweets/",
-    //   data: tweetQueryString,
-    // });
-    
     //prevents default http POST from the tweet form
     event.preventDefault();
+
+    if ($('#counter').text() < 0) {
+      alert("Sorry, you cannot submit more than 140 characters!");
+    } 
+    else if ($('#counter').text() >= 140) {
+      alert("Sorry, you cannot submit an empty tweet!")
+    } else {
+      
+      
+      
+   //   $('#tweet-sbmit').trigger("reset");
+      
+      
+      const tweetEntered = $(this);
+      //serialize tweet text for backend consumption
+      tweetQueryString = tweetEntered.serialize();
+      
+      //send AJAX POST request
+      // $post("/tweets/", tweetQueryString);
+      
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: tweetQueryString,
+      });
+
+      $('#tweet-submit')[0].reset();
+      $('#counter').text(140);
+    }
   });
+
+  // const loadTweets = function(data) {
+  //   $.ajax({
+  //     type: "GET",
+  //     url: "/tweets",
+  //     data: data,
+  //   }).done(function (tweetData) {
+  //     renderTweets(tweetData);
+  // });
+
+  const loadTweets = function() {
+    $.get("/tweets", function(tweetData) {
+      renderTweets(tweetData);
+    });
+  };
+  loadTweets();
+
 });
